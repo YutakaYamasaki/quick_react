02b4f75806f89241c8adc4d17396efd170839e3f
YutakaYamasaki <uminchuvirtual@gmail.com>
2018-11-30 21:13:14 +0800

    Chapter 2

--- src/index.js
+++ src/index.js
@@ -4,10 +4,40 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const name = 'yamasaki';
+const str = 'こんにちは、世界!<br/>さようなら、私';
+const url = 'https:// wings. msn. to/';
+const style = {color: 'Red', backgroundColor: 'Yellow'};
+const attrs ={
+    src:'http://www.wings.msn.to/image/wings.jpg',
+    alt:'',
+    title:'Wings'
+};
+const tag = React.createElement(
+    'div',
+    {className:'main'},
+    React.createElement(
+        'h1',
+        null,
+        'ようこそ、${name}へ'
+    ),
+    React.createElement(
+        'img',
+        {src:'http:// www. wings. msn. to/ image/ wings. jpg'}
+    ),
+    'こんにちは、世界'
+);
 ReactDOM.render(
-    <div>
-        <h3>現在の時間は、{(new Date()).toLocaleString()}</h3>
-    </div>, document.getElementById('root'));
+    <React.Fragment>
+        <p>{tag}</p>
+        <img {...attrs} />
+        <p dangerouslySetInnerHTML={{__html: str}}></p>
+        <a href={url}>サポート</a>
+        <p style={style}>Wings</p>
+        <p>こんにちは、{'$'+name+'$'}さん</p>
+        <p>現在の日時は、{new Date().toLocaleString()}です</p>
+        <p>2の3乗は、{2**3}です。</p>
+    </React.Fragment>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
